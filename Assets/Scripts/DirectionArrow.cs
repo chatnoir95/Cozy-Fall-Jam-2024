5b86098f0b0fad16fb5ec22981ecfded5080b006
Osama Hussein <osamahusseinn2@gmail.com>
2024-10-25 14:16:53 -0400

    The direction arrows will rotate to look at the store marked for delivery

--- Assets/Scripts/DirectionArrow.cs
+++ Assets/Scripts/DirectionArrow.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static UnityEngine.GraphicsBuffer;
 
 public class DirectionArrow : MonoBehaviour
@@ -16,14 +17,68 @@ public class DirectionArrow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (store.transform.position.x < Player.playerCharacters.transform.position.x)
+        /* If the store's y position is equal to the player's y position and player's x position is greater than
+        the store's x position */
+        if (store.transform.position.x < Player.playerCharacters.transform.position.x &&
+            store.transform.position.y == Player.playerCharacters.transform.position.y)
         {
             transform.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
         }
 
-        if (store.transform.position.x > Player.playerCharacters.transform.position.x)
+        /* If the store's y position is equal to the player's y position and player's x position is less than
+        the store's x position */
+        else if (store.transform.position.x > Player.playerCharacters.transform.position.x &&
+            store.transform.position.y == Player.playerCharacters.transform.position.y)
         {
             transform.rotation = Quaternion.Euler(0.0f, 0.0f, -90.0f);
+        }
+
+        /* If the store's y position is less than the player's y position and player's x position is greater than
+        the store's x position */
+        else if (store.transform.position.x < Player.playerCharacters.transform.position.x &&
+            store.transform.position.y < Player.playerCharacters.transform.position.y)
+        {
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, -135.0f);
+        }
+
+        /* If the store's y position is greater than the player's y position and player's x position is greater than
+        the store's x position */
+        else if (store.transform.position.x < Player.playerCharacters.transform.position.x &&
+            store.transform.position.y > Player.playerCharacters.transform.position.y)
+        {
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 135.0f);
+        }
+
+        /* If the store's y position is less than the player's y position and player's x position is less than
+        the store's x position */
+        else if (store.transform.position.x > Player.playerCharacters.transform.position.x &&
+            store.transform.position.y < Player.playerCharacters.transform.position.y)
+        {
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 135.0f);
+        }
+
+        /* If the store's y position is greater than the player's y position and player's x position is less than
+        the store's x position */
+        else if (store.transform.position.x > Player.playerCharacters.transform.position.x &&
+            store.transform.position.y > Player.playerCharacters.transform.position.y)
+        {
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, -135.0f);
+        }
+
+        /* If the store's y position is less than the player's y position and player's x position is equal to
+        the store's x position */
+        else if (store.transform.position.x == Player.playerCharacters.transform.position.x &&
+            store.transform.position.y < Player.playerCharacters.transform.position.y)
+        {
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 180.0f);
+        }
+
+        /* If the store's y position is greater than the player's y position and player's x position is equal to
+        the store's x position */
+        else if (store.transform.position.x == Player.playerCharacters.transform.position.x &&
+            store.transform.position.y > Player.playerCharacters.transform.position.y)
+        {
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
         }
     }
 }
